6b4993a221340b78207d1d45f77f334fb255491c
ShaJiuquan <126124028+ShaJiuquan@users.noreply.github.com>
2025-12-14 04:24:08 +0800

    feat: 添加 GitHub Pages 部署配置

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ppms_newVersion_1.0/',
+  base: '/ppms-website/',
   build: {
     outDir: 'dist'
   }
