d944bd555f78c53180f209d7b1121f2b597edb2f
ShaJiuquan <126124028+ShaJiuquan@users.noreply.github.com>
2025-12-17 23:27:31 +0800

    配置网络穿透

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Download from './pages/Download';
 import './App.css';
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/download" element={<Download />} />
+        <Route path="/ws-demo" element={<Navigate to="/ppms_ws_demo/" replace />} />
       </Routes>
     </Router>
   );
